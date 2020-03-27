f9f2b6f2adabe266c6c25c2bedfcbf279149cf5b
John Doe <nadia643@gmail.com>
2020-03-27 10:02:32 +0000

    added quotes as an array

--- quote.js
+++ quote.js
@@ -1,3 +1,16 @@
+let quotes = [
+    "Logic is the beginning of wisdom, not the end. -- Spock",
+    "Highly illogical. -- Spock",
+    "Live long, and prosper. -- Spock",
+    "Things are only impossible until they're not. -- Captain Jean-Luc Picard",
+    "Insufficient facts always invite danger. -- Spock",
+    "Compassion: that's the one things no machine ever had. Maybe it's the one thing that keeps men ahead of them. -- Dr. McCoy",
+    "We prefer to help ourselves. We make mistakes, but we're human--and maybe that's the word that best explains us. -- Captain James T. Kirk"
+]
+
+let quote = quotes[Math.floor(Math.random() * quotes.length)];
+
 function newQuote() {
-    document.getElementById("quote").innerHTML = "new quote"
+    document.getElementById("quote").innerHTML = quote;
 }
+
